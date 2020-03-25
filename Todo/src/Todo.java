d2d20b038da1e468d894fcf6ae4f3bddc46680c0
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-03-25 13:03:55 +0100

    print usage added

--- Todo/src/Todo.java
+++ Todo/src/Todo.java
@@ -8,34 +8,18 @@ import java.util.Map;
 public class Todo {
 
   public static void main(String[] args) {
+    printUsage();
+  }
 
-    int counter = 1;
-    for (String arg : args) {
-      System.out.println(counter + arg);
-      counter++;
-    }
-    HashMap<String, String> printUsage = new HashMap<>();
-
-    printUsage.put("-l","List all the tasks");
-    printUsage.put("-a","Adds anew task");
-    printUsage.put("-r","Removes a task");
-    printUsage.put("-c","Completes a task");
-
-    System.out.println(printUsage);
-
-
-  /*List <String> data = new ArrayList<>();
-    data.add("Command line Todo application\n");
-    data.add("==============================\n");
-    System.out.println();
-    data.add("Command line arguments\n");
-    data.add("\t-l\tLists all arguments\n");
-    data.add("\t-a\tAdds a new task\n");
-    data.add("\t-r\tRemoves a task\n");
-    data.add("\t-c\tCompletes a task\n");
-    System.out.println(data.toString());*/
-
-
+  public static void printUsage(){
+    String list = "Command line Todo application\n";
+    list = list.concat("=============================\n");
+    list = list.concat(" \n");
+    list = list.concat("\t-l\tLists all arguments\n");
+    list = list.concat("\t-a\tAdds a new task\n");
+    list = list.concat("\t-r\tRemoves a task\n");
+    list = list.concat("\t-c\tCompletes a task\n");
+    System.out.println(list);
   }
 
 }
