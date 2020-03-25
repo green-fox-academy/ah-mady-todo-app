66db0f4cf5e1278cf3a7f86d7cd5191d9196e4f0
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-03-25 14:40:43 +0100

    List tasks

--- Todo/src/Todo.java
+++ Todo/src/Todo.java
@@ -1,17 +1,24 @@
-import java.lang.reflect.Array;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Todo {
 
-  public static void main(String[] args) {
-    printUsage();
+  public static void main(String[] args) throws IOException {
+
+    if (args[0].equals("-l")){
+      int count = 1;
+      for (String arg: todoList()) {
+        System.out.println(count + " - " + arg);
+        count++;
+      }
+    }
   }
 
-  public static void printUsage(){
+  public static void printUsage() {
     String list = "Command line Todo application\n";
     list = list.concat("=============================\n");
     list = list.concat(" \n");
@@ -20,6 +27,20 @@ public class Todo {
     list = list.concat("\t-r\tRemoves a task\n");
     list = list.concat("\t-c\tCompletes a task\n");
     System.out.println(list);
+  }
+
+  public static List<String> todoList(){
+    Path path = Paths.get("src/todo-list.txt");
+    List<String> lines = new ArrayList<>();
+    try{
+      lines = Files.readAllLines(path);
+      for (String line: lines) {
+        System.out.println(line);
+      }
+    }catch (IOException e){
+      System.out.println("File not found!");
+    }
+    return lines;
   }
 
 }
