af6b445f97fbb34b5eaa492bbaf1643a990bb565
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-03-25 11:40:34 +0100

    checking

--- Todo/src/Todo.java
+++ Todo/src/Todo.java
@@ -1,2 +1,11 @@
 public class Todo {
+
+  public static void main(String[] args) {
+
+    int counter = 1;
+    for (String arg:args) {
+      System.out.println(counter + arg);
+      counter++;
+    }
+  }
 }
